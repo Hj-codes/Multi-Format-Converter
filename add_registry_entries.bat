4ae1e5c6932a0e02400ceea4cf3568a42866abff
Harsh Jain <131331439+Hj-codes@users.noreply.github.com>
2024-01-13 22:53:09 +0530

    Update add_registry_entries.bat
    changed a typo in batch file

--- add_registry_entries.bat
+++ add_registry_entries.bat
@@ -4,14 +4,14 @@
 set FILE_FORMATS=.gif .jpg .png .bmp .jpeg .mp4 .avi .mkv .mov .mp3 .wav
 
 :: Set the MUIVerb value
-reg add "HKEY_CLASSES_ROOT\*\shell\Convert1" /v MUIVerb /t REG_SZ /d Convert /f
+reg add "HKEY_CLASSES_ROOT\*\shell\Convert" /v MUIVerb /t REG_SZ /d Convert /f
 
 :: Set the SubCommands value
-reg add "HKEY_CLASSES_ROOT\*\shell\Convert1" /v SubCommands /t REG_SZ /d "" /f
+reg add "HKEY_CLASSES_ROOT\*\shell\Convert" /v SubCommands /t REG_SZ /d "" /f
 
 :: Loop through each file format and create registry entries
 for %%F in (%FILE_FORMATS%) do (
-  reg add "HKEY_CLASSES_ROOT\*\shell\Convert1\shell\%%F\command" /ve /t REG_SZ /d "\"C:\Path\To\Your\Python\python.exe\" \"C:\Path\To\Your\Script\Converter.py\" \"%%1\" \"%%F\"" /f
+  reg add "HKEY_CLASSES_ROOT\*\shell\Convert\shell\%%F\command" /ve /t REG_SZ /d "\"C:\Path\To\Your\Python\python.exe\" \"C:\Path\To\Your\Script\Converter.py\" \"%%1\" \"%%F\"" /f
 )
 
 echo Registry entries added successfully.
